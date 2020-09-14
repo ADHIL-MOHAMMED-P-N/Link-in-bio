65890f9d7d63bab71d6ebe065b90a2af7b749dce
Adhil-Mohammed-P-N <adhilmohammedpn@gmail.com>
2020-09-14 21:53:40 +0530

    Updates

---
+++ precache-manifest.1c6506815e79e1ca7fc3e12d2880241e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "39849029747f2b1e6c316012e2b69ece",
+    "url": "/Link-in-bio/index.html"
+  },
+  {
+    "revision": "b45d6f9e6b8c0d7d2fb8",
+    "url": "/Link-in-bio/static/css/main.6b3ac71a.chunk.css"
+  },
+  {
+    "revision": "5fb20418b5c8788c7506",
+    "url": "/Link-in-bio/static/js/2.f1a936b8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Link-in-bio/static/js/2.f1a936b8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b45d6f9e6b8c0d7d2fb8",
+    "url": "/Link-in-bio/static/js/main.9cc656d5.chunk.js"
+  },
+  {
+    "revision": "93ad169ff8a808caf81a",
+    "url": "/Link-in-bio/static/js/runtime-main.7644866b.js"
+  }
+]);
